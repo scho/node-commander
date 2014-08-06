1dc1c8274f68e07bfae700ad6af9b8535666dcb9
Georg Meyer <georgmeyer83@googlemail.com>
2014-08-06 15:29:56 +0200

    Add first draft and readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Node Commander
 ==============
+Node Commander is a very simple web application to browse a directory tree, similar to the well-known [Northon Commander](http://de.wikipedia.org/wiki/Norton_Commander).
+
+Usage
+-----
+```node index.js```.
---
+++ index.js
@@ -0,0 +1,80 @@
+
+var http = require('http'),
+    fs = require('fs'),
+    doT = require('dot'),
+    root = "/",
+    path,
+    nodeCompareFn = function(otherNode) {
+        if(!this.isDirectory && otherNode.isDirectory){
+            return 1;
+        }
+        if(!otherNode.isDirectory && this.isDirectory){
+            return -1;
+        }
+        return this.localeLowerCaseName.localeCompare(otherNode.localeLowerCaseName);
+    },
+    Node = function(name, stats) {
+        this.name = name;
+        this.localeLowerCaseName = name.toLocaleLowerCase();
+        this.selected = false;
+        this.isDirectory = stats.isDirectory();
+        this.mtime = stats.mtime;
+        this.size = stats.size;
+        this.compare = nodeCompareFn;
+    },
+    indexTemplate = fs.readFileSync('views/index.dot', { encoding: 'utf-8' }),
+    indexTemplateFn;
+
+doT.templateSettings.strip = false;
+
+indexTemplateFn = doT.template(indexTemplate);
+
+http.createServer(function(request, response) {
+    path = root + decodeURIComponent(request.url);
+
+    path = path.replace(/\/+/, "/");
+
+    if(!path.match(/\/$/)){
+        response.writeHead(302, {
+          'Location': request.url + "/"
+        });
+        response.end();
+        return;
+    }
+
+    if(!fs.existsSync(path)) {
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+
+    fs.readdir(path, function(error, files) {
+        response.writeHead(200);
+        var nodes = [],
+            htmlNodes = '';
+
+        files = files || [];
+
+        for(var i = 0; i < files.length; i += 1){
+            node = new Node(files[i], fs.lstatSync(path + files[i]));
+            nodes.push(node);
+        }
+
+        nodes.sort(function(a,b) {
+            return a.compare(b);
+        });
+
+        if(path !== root) {
+            nodes.splice(0, 0, new Node("..", fs.lstatSync(path + "..")));            
+        }
+
+        if(nodes.length > 0) {
+            nodes[0].selected = true;
+        }
+        
+        response.write(indexTemplateFn({ nodes: nodes }));
+        response.end();
+    });
+
+}).listen(8080);
+console.log('Listening on port 8080...');
