8d8c8e7638cca14437f6b01ae69d43699125b0f5
Georg Meyer <georgmeyer83@googlemail.com>
2014-08-06 15:51:01 +0200

    Make file exists check async

--- index.js
+++ index.js
@@ -42,38 +42,40 @@ http.createServer(function(request, response) {
         return;
     }
 
-    if(!fs.existsSync(path)) {
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-
-    fs.readdir(path, function(error, files) {
-        response.writeHead(200);
-        var nodes = [],
-            htmlNodes = '';
-
-        files = files || [];
-
-        for(var i = 0; i < files.length; i += 1){
-            node = new Node(files[i], fs.lstatSync(path + files[i]));
-            nodes.push(node);
+    fs.exists(path, function(exists){
+        if(!exists) {
+            response.writeHead(404);
+            response.end();
+            return;
         }
 
-        nodes.sort(function(a,b) {
-            return a.compare(b);
+        fs.readdir(path, function(error, files) {
+            response.writeHead(200);
+            var nodes = [],
+                htmlNodes = '';
+
+            files = files || [];
+
+            for(var i = 0; i < files.length; i += 1){
+                node = new Node(files[i], fs.lstatSync(path + files[i]));
+                nodes.push(node);
+            }
+
+            nodes.sort(function(a,b) {
+                return a.compare(b);
+            });
+
+            if(path !== root) {
+                nodes.splice(0, 0, new Node("..", fs.lstatSync(path + "..")));
+            }
+
+            if(nodes.length > 0) {
+                nodes[0].selected = true;
+            }
+
+            response.write(indexTemplateFn({ nodes: nodes }));
+            response.end();
         });
-
-        if(path !== root) {
-            nodes.splice(0, 0, new Node("..", fs.lstatSync(path + "..")));            
-        }
-
-        if(nodes.length > 0) {
-            nodes[0].selected = true;
-        }
-        
-        response.write(indexTemplateFn({ nodes: nodes }));
-        response.end();
     });
 
 }).listen(8080);
